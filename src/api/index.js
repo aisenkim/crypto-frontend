e98bb162e9c6b4ce2aa5e8ac0d553f23bae40de6
Aisen Kim <aisenkim15@gmail.com>
2022-02-22 16:38:46 -0500

    edited route

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 const api = axios.create({
   // baseURL: 'http://localhost:8080/api/v1'
-  baseURL: "http://localhost:9000/api/v1", // for deployment
+  //baseURL: "http://localhost:9000/api/v1", // for deployment
+  baseURL: "http://aisencode.com/api/v1", // for deployment
 });
 
 export const getBuySellPrice = (exchange, symbol) =>
